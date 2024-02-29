46c600d52a49b1a159af4e8e2a6ad48a7feaefdf
Mansfera <mansferagg@gmail.com>
2024-02-29 19:20:42 +0200

    fix encryption

--- script.js
+++ script.js
@@ -4,48 +4,31 @@ var access_token = params.get('access_token');
 var allowedPart = params.get('allowedPart');
 
 var decrypted_token = Number(CryptoJS.AES.decrypt(access_token, "lag@history").toString(CryptoJS.enc.Utf8));
-var decrypted_blocks = Number(CryptoJS.AES.decrypt(allowedPart, "lag@history").toString(CryptoJS.enc.Utf8));
+var decrypted_blocks = CryptoJS.AES.decrypt(allowedPart, "lag@history").toString(CryptoJS.enc.Utf8);
 
 if (params == null) {
     window.location = "./expired_token.html"
 }
-if (decrypted_token < Date.now()) {
-    // window.location = "./expired_token.html"C
+if (decrypted_token > Date.now() / 1000) {
+    window.location = "./expired_token.html?"+params
 } else {
-    if (decrypted_blocks.search("1")) {
-        document.getElementById("b1").classList.remove("blocked")
-    } else {
-        document.removeChild(document.getElementById("b1"))
+    for (var i = 1; i < 6; i++){
+        var block = document.getElementById("b"+i);
+        if (decrypted_blocks.includes(i)) {
+            block.classList.remove("blocked")
+        } else {
+            while (block.firstChild) {
+                block.removeChild(block.firstChild);
+            }
+        }
     }
 
-    if (decrypted_blocks.search("2")) {
-        document.getElementById("b2").classList.remove("blocked")
+    var block = document.getElementById("NMT");
+    if (decrypted_blocks.includes("6")) {
+        block.classList.remove("blocked")
     } else {
-        document.removeChild(document.getElementById("b2"))
+        while (block.firstChild) {
+            block.removeChild(block.firstChild);
+        }
     }
-
-    if (decrypted_blocks.search("3")) {
-        document.getElementById("b3").classList.remove("blocked")
-    } else {
-        document.removeChild(document.getElementById("b3"))
-    }
-
-    if (decrypted_blocks.search("4")) {
-        document.getElementById("b4").classList.remove("blocked")
-    } else {
-        document.removeChild(document.getElementById("b4"))
-    }
-
-    if (decrypted_blocks.search("5")) {
-        document.getElementById("b5").classList.remove("blocked")
-    } else {
-        document.removeChild(document.getElementById("b5"))
-    }
-
-    if (decrypted_blocks.search("6")) {
-        document.getElementById("NMT").classList.remove("blocked")
-    } else {
-        document.removeChild(document.getElementById("NMT"))
-    }
-
 }
