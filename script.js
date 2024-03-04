8e67e69b561045238274feb8bfe2aa9a336f8cee
Mansfera <mansferagg@gmail.com>
2024-03-04 12:06:04 +0200

    try main screen run

--- script.js
+++ script.js
@@ -65,3 +65,12 @@ if (decrypted_token < Date.now()) {
       block.appendChild(oImg);
   }
 }
+
+
+$(".blocked").on('mouseover', function(){
+  var offset = $(this).offset();
+  var goX = Math.random() < 0.5 ? -1 : 1;
+  var goY = Math.random() < 0.5 ? -1 : 1;
+  $(this).css('top', offset.top + 20 * goY);
+  $(this).css('left', offset.left + 20 * goX);
+});
